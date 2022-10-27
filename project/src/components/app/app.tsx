fda7c47b4658cc78216b5971271ef7effe982b17
YourfriendRND <kupeirc@gmail.com>
2022-10-27 17:42:59 +0300

    Добавил Helmet, добавил компонент Header

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import { AppRoute, UserAuthStatus } from '../../const';
 import StartScreen from '../../pages/start-screen/start-screen';
 import AuthScreen from '../../pages/auth-screen/auth-screen';
@@ -13,21 +14,22 @@ type AppProp = {
 
 function App({ placeCount }: AppProp): JSX.Element {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={AppRoute.Main} element={<StartScreen placeCount={placeCount}/>} />
-        <Route path={AppRoute.Login} element={<AuthScreen />} />
-        <Route path={AppRoute.Favorites} element={
-          <PrivateRoute userAuthStatus={UserAuthStatus.Unknown}>
-            <FavoritesScreen />
-          </PrivateRoute>
-        }
-        />
-        <Route path={AppRoute.Room} element={<RoomScreen />} />
-        <Route path='*' element={<UnexistScreen />} />
-      </Routes>
-      ;
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Main} element={<StartScreen placeCount={placeCount} />} />
+          <Route path={AppRoute.Login} element={<AuthScreen />} />
+          <Route path={AppRoute.Favorites} element={
+            <PrivateRoute userAuthStatus={UserAuthStatus.Unknown}>
+              <FavoritesScreen />
+            </PrivateRoute>
+          }
+          />
+          <Route path={AppRoute.Room} element={<RoomScreen />} />
+          <Route path='*' element={<UnexistScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
